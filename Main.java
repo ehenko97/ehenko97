a86f998a6ac50ac2f278078f4126d3e773d3e201
Pavel <105103021+ehenko97@users.noreply.github.com>
2022-06-23 16:59:42 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,105 @@
+import java.lang.String;
+public class Main {   
+    public static void main(String[] args) {
+    //TODO: просуммировать числа в диапозоне от 1 - 5
+        /*
+            1 + 2 + 3 + 4 + 5 = 15
+            1 + 2 = 3
+            3 + 3 = 6
+            6 + 4 = 10
+            10 + 5 =15
+            int sum = 1 + 2;
+            int sum1 = sum + 3;
+            int sum2 = sum1 + 4;
+            int result = sum2 + 5;
+            System.out.println(result)
+        byte num = 23;
+        int num_1 = 34656748;
+        float num_2 = 23.45f;
+        double num_3 = 234.34553665768797d;
+        boolean isTrue = false;
+        char sym = 'R';
+        String str = "Hello World!";
+        System.out.print(str);
+        byte num =12;
+        short num_0 = 1999;
+        int num_1 = 34656748;
+        long num_2 =1111111111;
+        float num_3 = 23.45f;
+        double num_4 = 234.34553665768797d;
+        char sym = 'R';
+        boolean isTrue = false;
+        String str = "   J    a  v     v  a    ";
+        System.out.println(str);
+        String str_1 = "   J   a a  v   v  a a   ";
+        System.out.println(str_1);
+        String str_2 = "J  J  aaaaa  V V  aaaaa  ";
+        System.out.println(str_2);
+        String str_3 = " JJ  a     a  V  a     a";
+        System.out.println(str_3);
+        byte num_x= 100;
+        short num_y = 200;
+        System.out.println(num_x + num_y);
+        short num_f = 300;
+        byte num_r = 100;
+        System.out.println(num_f / num_r);
+        short num_t = 3;
+        byte num_p = 11;
+        System.out.println(num_t * num_p);
+        short num_l = 300;
+        byte num_m = 33;
+        System.out.println(num_l - num_m);
+        i = 33;
+        ii = 267;
+        System.out.println("Первоначальное значение");
+        byte min_5 = -128;
+        System.out.println(min_5);
+        short min_6 = -32768;
+        System.out.println(min_6);
+        int min_7 = -2147483648;
+        System.out.println(min_7);
+        long min_8 = -9223372036854775808L;
+        System.out.println(min_8);
+        System.out.println(         );
+        byte max_5 = 127;
+        System.out.println(max_5);
+        short max_6 = 32767;
+        System.out.println(max_6);
+        int max_7 = 2147483647;
+        System.out.println(max_7);
+        long max_8 = 9223372036854775807L;
+        System.out.println(max_8);
+        System.out.println("Значение после инкремента");
+        byte min_1 = -128;
+        System.out.println(--min_1);
+        short min_2 = -32768;
+        System.out.println(--min_2);
+        int min_3 = -2147483648;
+        System.out.println(--min_3);
+        long min_4 = -9223372036854775808L;
+        System.out.println(--min_4);
+        System.out.println("Значение после декремента");
+        byte max_1 = 127;
+        System.out.println(++max_1);
+        short max_2 = 32767;
+        System.out.println(++max_2);
+        int max_3 = 2147483647;
+        System.out.println(++max_3);
+        long max_4 = 9223372036854775807L;
+        System.out.println(++max_4);
+        */
+        double num_1 = 1.1d;
+        double num_2 = 2.2d;
+        double temp;
+        System.out.println(" num_1 =" + num_1 + "num_2 =" + num_2);
+        double num_1 = temp;
+        double num_2 = 1.1d;
+        double temp = 2.2d;
+        System.out.println(num_2 = + num_2 + num_1 = + num_2);
+    /*
+        System.out.println(num_1 + num_2);
+        System.out.println(num_1 - num_2);
+        System.out.println(num_1 + num_2 - (num_1 = num_2));
+        */
+    }
+}
